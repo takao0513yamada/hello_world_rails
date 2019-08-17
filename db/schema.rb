3d87480e27c8861137c889d529ff5213b876b6b0
taka president <life.i.s.w---@i.softbank.jp>
2019-08-17 21:13:20 +0900

    add articles_status
    記事のステータス設定と下書き記事はユーザー本人のみ閲覧可能とする設定の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_203638) do
+ActiveRecord::Schema.define(version: 2019_08_17_060746) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_203638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
